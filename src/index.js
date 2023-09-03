3de2c2f645e5615b8d8c9eae7305522faf7170a6
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-09-03 19:39:24 -0300

    fetch data from api weather

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+let local = 'london'
+
+async function weather() {
+    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=b3f95362dd9b4e10a9b180955230309&q=" + `${local}` + "&aqi=no", {mode: 'cors'})
+    const weatherData = await response.json();
+    console.log(weatherData.current.temp_c)
+}
+
+weather()
