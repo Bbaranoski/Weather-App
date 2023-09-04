1d5f44d2caa441ac54991f8420b5c9cdb9427b78
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-09-04 13:48:30 -0300

    debugging and running the obj constructor

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-import './style.css'
+import "./style.css";
 
-let local = 'london'
+let local = "london";
 
 async function weather() {
-    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=b3f95362dd9b4e10a9b180955230309&q=" + `${local}` + "&aqi=no", {mode: 'cors'})
-    const weatherData = await response.json();
-    console.log(weatherData)
-    const c = weatherData.current.temp_c
-    const f = weatherData.current.temp_f
-    const feelsC = weatherData.current.feelslike_c
-    const feelsF = weatherData.current.feelslike_f
-    const humidity = weatherData.current.humidity
-    const place = weatherObj(c, f, feelsC, feelsF, humidity)
+  const response = await fetch(
+    "http://api.weatherapi.com/v1/current.json?key=b3f95362dd9b4e10a9b180955230309&q=" +
+      `${local}` +
+      "&aqi=no",
+    { mode: "cors" },
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+  const c = weatherData.current.temp_c;
+  const f = weatherData.current.temp_f;
+  const feelsC = weatherData.current.feelslike_c;
+  const feelsF = weatherData.current.feelslike_f;
+  const humidity = weatherData.current.humidity;
+  const data = new weatherObj(c, f, feelsC, feelsF, humidity)
+  console.log(data.c)
 }
 
-weather()
+weather();
 
-const input = document.getElementById('local')
-const form = document.querySelector('form')
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    local = input.value
-    input.value = ''
-    weather()
-})
+const input = document.getElementById("local");
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  local = input.value;
+  input.value = "";
+  weather();
+});
 
-function weatherObj(c, f, feelsC, feelsF, humidity){
-    this.c = c
-    this.f = f
-    this.feelsC = feelsC
-    this.feelsF = feelsF
-    this.humidity = humidity
+function weatherObj(c, f, feelsC, feelsF, humidity) {
+  this.c = c;
+  this.f = f;
+  this.feelsC = feelsC;
+  this.feelsF = feelsF;
+  this.humidity = humidity;
 }
