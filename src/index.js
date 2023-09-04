8f3da55e3d7fb317d3b8b2570820555957a02ee6
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-09-03 23:32:10 -0300

    begin to make a weather obj constructo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ let local = 'london'
 async function weather() {
     const response = await fetch("http://api.weatherapi.com/v1/current.json?key=b3f95362dd9b4e10a9b180955230309&q=" + `${local}` + "&aqi=no", {mode: 'cors'})
     const weatherData = await response.json();
-    console.log(weatherData.current.temp_c)
+    console.log(weatherData)
+    const c = weatherData.current.temp_c
+    const f = weatherData.current.temp_f
+    const feelsC = weatherData.current.feelslike_c
+    const feelsF = weatherData.current.feelslike_f
+    const humidity = weatherData.current.humidity
+    const place = weatherObj(c, f, feelsC, feelsF, humidity)
 }
 
 weather()
@@ -15,5 +21,14 @@ const form = document.querySelector('form')
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     local = input.value
+    input.value = ''
     weather()
 })
+
+function weatherObj(c, f, feelsC, feelsF, humidity){
+    this.c = c
+    this.f = f
+    this.feelsC = feelsC
+    this.feelsF = feelsF
+    this.humidity = humidity
+}
